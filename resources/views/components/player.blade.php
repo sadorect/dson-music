bd1c2b3095b9a1ab31577f80fe6e53aaa165448e
Tosin Omojola <sadorect@gmail..com>
2025-01-06 16:52:19 +0000

    music player enahnced

--- resources/views/components/player.blade.php
+++ resources/views/components/player.blade.php
@@ -1,21 +1,18 @@
-<div class="dson-player fixed bottom-0 w-full bg-black text-white">
-    <div class="container mx-auto flex items-center justify-between p-4">
-        <div class="track-info flex items-center">
-            <img id="track-artwork" class="w-16 h-16 rounded" src="" alt="Track Artwork">
-            <div class="ml-4">
-                <h4 id="track-title" class="text-lg font-bold"></h4>
-                <p id="track-artist" class="text-sm text-gray-400"></p>
+<div class="fixed bottom-0 w-full bg-black text-white p-4 dson-player">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="track-info">
+            <h4 class="font-bold">Currently Playing</h4>
+            <p class="text-sm opacity-75">Artist Name</p>
+        </div>
+        <div class="flex items-center gap-4">
+            <button class="dson-btn-player">⏮</button>
+            <button class="dson-btn-player">▶</button>
+            <button class="dson-btn-player">⏭</button>
+        </div>
+        <div class="w-1/3">
+            <div class="h-1 bg-gray-700 rounded">
+                <div class="h-1 bg-red-600 rounded" style="width: 30%"></div>
             </div>
-        </div>
-        
-        <div class="player-controls flex items-center">
-            <button class="dson-btn-player mx-2">Previous</button>
-            <button class="dson-btn-player mx-2">Play/Pause</button>
-            <button class="dson-btn-player mx-2">Next</button>
-        </div>
-        
-        <div class="volume-control flex items-center">
-            <input type="range" min="0" max="100" value="100" class="w-32">
         </div>
     </div>
 </div>
