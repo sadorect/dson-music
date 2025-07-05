094acdb8853f822949626f8f76c90c458a777d9c
sirhmvfx <sirhmvfx@gmail.com>
2025-07-05 19:20:23 +0100

    added global theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "primary-color": "#ffcb01",
+                "bg": "#121212"
+            },
         },
     },
 
