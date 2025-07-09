36dfd8ffa60f469c39632e0618ba498dbae25aae
sirhmvfx <sirhmvfx@gmail.com>
2025-07-09 19:25:51 +0100

    added trending song component

---
+++ resources/views/components/home/trending.blade.php
@@ -0,0 +1,66 @@
+
+    <div class="w-full">
+        <div class="w-full">
+            <div class="flex items-center justify-between mb-6">
+                <h2 class="text-2xl font-bold">Trending songs</h2>
+                <a href="#" class="text-sm text-gray-400 hover:text-white">Show all</a>
+            </div>
+
+            <div class="flex gap-4 overflow-x-scroll w-full">
+                <x-song-card 
+                    title="Gold" 
+                    artist="J Hus, Asake" 
+                    image="https://link-to-image/gold.jpg" />
+
+                <x-song-card 
+                    title="Ewo" 
+                    artist="Famous Pluto, Shallipopi, Zerrydl" 
+                    image="https://link-to-image/ewo.jpg" />
+
+                <x-song-card 
+                    title="Don't Let Me Drown" 
+                    artist="Burna Boy, F1 The Album" 
+                    image="https://link-to-image/drown.jpg" />
+
+                <x-song-card 
+                    title="Gang" 
+                    artist="Ayo Maff, Seyi Vibez" 
+                    image="https://link-to-image/gang.jpg" />
+
+                <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+                
+                 <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+
+                 <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+
+                
+                <x-song-card 
+                    title="Gang" 
+                    artist="Ayo Maff, Seyi Vibez" 
+                    image="https://link-to-image/gang.jpg" />
+
+                <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+                
+                 <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+
+                 <x-song-card 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+            </div>
+        </div>
