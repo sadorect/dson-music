687793e76bda9b8f1f98451667946f4918bdefc0
sirhmvfx <sirhmvfx@gmail.com>
2025-07-11 19:30:48 +0100

    fix general settings'

---
+++ app/Models/GeneralSettings.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GeneralSettings extends Model
+{
+    protected $table = 'general_settings';
+    
+    protected $casts = [
+        'maintenance_mode' => 'boolean',
+        'enable_registration' => 'boolean',
+        'max_upload_size' => 'integer',
+        'hero_slides' => 'array',
+        'social_links' => 'array',
+    ];
+
+    protected $fillable = [
+        'site_name',
+        'maintenance_mode',
+        'max_upload_size',
+        'hero_slides',
+        'site_description',
+        'contact_email',
+        'social_links',
+        'enable_registration',
+        'footer_text',
+        'logo_desktop_path',
+        'logo_desktop_url',
+        'logo_mobile_path',
+        'logo_mobile_url',
+        'favicon_path',
+        'favicon_url',
+    ];
+}
