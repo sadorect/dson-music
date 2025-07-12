5d29d900b37e3e630e61bbbd3e046b8e6234ba1f
sirhmvfx <sirhmvfx@gmail.com>
2025-07-12 19:31:07 +0100

    fix notifications

---
+++ app/Models/Notification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $fillable = [
+        'notifiable_id',
+        'notifiable_type',
+        'type',
+        'data',
+        'read_at'
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'read_at' => 'datetime'
+    ];
+
+    public function notifiable()
+    {
+        return $this->morphTo();
+    }
+}
