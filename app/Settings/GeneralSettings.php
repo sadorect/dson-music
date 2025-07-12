cd93c50a528b5c9586c48832266506b357478e54
sirhmvfx <sirhmvfx@gmail.com>
2025-07-12 19:31:24 +0100

    reconfigured generala settigns

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -45,7 +45,22 @@ class GeneralSettings extends Settings
                     'image_path' => null,
                     'image_url' => 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80'
                 ]
-            ]
+            ],
+            'site_description' => 'A modern music streaming and distribution platform for independent artists',
+            'contact_email' => 'support@grinmusic.com',
+            'social_links' => [
+                'facebook' => 'https://facebook.com/grinmusic',
+                'twitter' => 'https://twitter.com/grinmusic',
+                'instagram' => 'https://instagram.com/grinmusic'
+            ],
+            'enable_registration' => true,
+            'footer_text' => '© 2025 GRIN Music. All rights reserved.',
+            'logo_desktop_path' => null,
+            'logo_desktop_url' => null,
+            'logo_mobile_path' => null,
+            'logo_mobile_url' => null,
+            'favicon_path' => null,
+            'favicon_url' => null
         ];
     }
 }
