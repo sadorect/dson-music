ba6b66dba86fc77bc53aeba50d600f5d22032dbb
sirhmvfx <sirhmvfx@gmail.com>
2025-07-26 15:15:16 +0100

    added popular artists card

---
+++ resources/views/components/home/popular-artists.blade.php
@@ -0,0 +1,62 @@
+
+<div class="w-full">
+        <div class="w-full">
+            <div class="flex items-center justify-between mb-2">
+                <h2 class="text-2xl font-bold text-white">Popular Artists</h2>
+                <a href="#" class="text-sm text-gray-400 hover:text-white">Show all</a>
+            </div>
+
+            <div class="flex gap-2 overflow-x-scroll scroll-smooth w-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden">
+                <x-artist 
+                    title="Gold" 
+                    artist="J Hus, Asake" 
+                    image="https://link-to-image/gold.jpg" />
+
+                <x-artist 
+                    title="Ewo" 
+                    artist="Famous Pluto, Shallipopi, Zerrydl" 
+                    image="https://link-to-image/ewo.jpg" />
+
+                <x-artist 
+                    title="Don't Let Me Drown" 
+                    artist="Burna Boy, F1 The Album" 
+                    image="https://link-to-image/drown.jpg" />
+
+                <x-artist 
+                    title="Gang" 
+                    artist="Ayo Maff, Seyi Vibez" 
+                    image="https://link-to-image/gang.jpg" />
+
+                <x-artist 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+                
+                 <x-artist 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+
+                
+                
+                <x-artist 
+                    title="Gang" 
+                    artist="Ayo Maff, Seyi Vibez" 
+                    image="https://link-to-image/gang.jpg" />
+
+                <x-artist 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+                
+                 <x-artist 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+
+                 <x-artist 
+                    title="Lighter" 
+                    artist="A7S, David Guetta, Wizkid" 
+                    image="https://link-to-image/lighter.jpg" />
+            </div>
+        </div>
