823006126ca11884e840b13ddb263dd351276118
sirhmvfx <sirhmvfx@gmail.com>
2025-07-31 09:01:02 +0100

    move library to the right, added footer in the main section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,27 +63,45 @@
         <!-- Page Content -->
         <main class=" h-[80%]">
             <div class="flex p-3 gap-8 h-full">
-
-
-
-                <div class="w-9/12 bg-white/5 p-6 rounded-lg overflow-y-scroll h-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden">
+                <div class="w-9/12 bg-white/5 rounded-lg overflow-y-scroll h-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden">
                     @yield('content')
+
+                    <x-footer />
                 </div>
 
-                <div class="w-3/12 bg-white/[5%] rounded-lg p-6 flex flex-col gap-4">
+                <div class="w-3/12 bg-white/[5%] rounded-lg p-6 flex flex-col gap-4 justify-between">
 
 
-                    <h1 class="text-white font-semibold text-lg">Your Library</h1>
-                    <div class="bg-black/10 p-6 rounded-lg">
-                        <h1 class="text-white font-semibold text-lg">Create your first playlist</h1>
-                        <p class="text-gray-400">Start creating your first playlist</p>
-                        <button class="bg-white my-2 text-black px-4 py-2 rounded-full">Create Playlist</button>
+                    <div class="flex flex-col gap-6">
+                        <h1 class="text-white font-semibold text-lg">Your Library</h1>
+                        <div class="bg-black/10 p-6 rounded-lg">
+                            <h1 class="text-white font-semibold text-lg">Create your first playlist</h1>
+                            <p class="text-gray-400">Start creating your first playlist</p>
+                            <button class="bg-white my-2 text-black px-4 py-2 rounded-full">Create Playlist</button>
+                        </div>
+
+                        <div class="bg-black/10 p-6 rounded-lg">
+                            <h1 class="text-white font-semibold text-lg">Let's find some podcasts to follow</h1>
+                            <p class="text-gray-400">We'll keep you updated on the latest episodes</p>
+                            <button class="bg-white my-2 text-black px-4 py-2 rounded-full">Find Podcasts</button>
+                        </div>
                     </div>
 
-                    <div class="bg-black/10 p-6 rounded-lg">
-                        <h1 class="text-white font-semibold text-lg">Let's find some podcasts to follow</h1>
-                        <p class="text-gray-400">We'll keep you updated on the latest episodes</p>
-                        <button class="bg-white my-2 text-black px-4 py-2 rounded-full">Find Podcasts</button>
+
+                    <div class=" flex flex-col gap-4">
+                        <div class="flex flex-wrap items-center gap-4 ">
+                            <a href="" class="text-xs text-white/50">Legal</a>
+                            <a href="" class="text-xs text-white/50">Safety and Privacy center</a>
+                            <a href="" class="text-xs text-white/50">Privacy Policy</a>
+                            <a href="" class="text-xs text-white/50">Cookies</a>
+                            <a href="" class="text-xs text-white/50">About Us</a>
+                            <a href="" class="text-xs text-white/50">Accessibility</a>
+                        </div>
+
+                        <button class="text-sm gap-3 w-fit border text-white/50 border-white/50 rounded-full px-4 py-2 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white/50 lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>    
+                        English
+                        </button>
                     </div>
 
                 </div>
