46f327549e89b93dc3bd528ae152537f7d6d884d
sirhmvfx <sirhmvfx@gmail.com>
2025-08-01 09:01:33 +0100

    made it a component to get drynamic data

--- resources/views/components/home/trending.blade.php
+++ resources/views/components/home/trending.blade.php
@@ -1,66 +1,57 @@
+@props(['title', 'tracks'])
 
+<div class="w-full">
     <div class="w-full">
-        <div class="w-full">
-            <div class="flex items-center justify-between mb-2">
-                <h2 class="text-2xl font-bold text-white">Trending songs</h2>
-                <a href="#" class="text-sm text-gray-400 hover:text-white">Show all</a>
-            </div>
-
-            <div class="flex gap-2 overflow-x-scroll scroll-smooth w-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden">
-                <x-song-card 
-                    title="Gold" 
-                    artist="J Hus, Asake" 
-                    image="https://link-to-image/gold.jpg" />
-
-                <x-song-card 
-                    title="Ewo" 
-                    artist="Famous Pluto, Shallipopi, Zerrydl" 
-                    image="https://link-to-image/ewo.jpg" />
-
-                <x-song-card 
-                    title="Don't Let Me Drown" 
-                    artist="Burna Boy, F1 The Album" 
-                    image="https://link-to-image/drown.jpg" />
-
-                <x-song-card 
-                    title="Gang" 
-                    artist="Ayo Maff, Seyi Vibez" 
-                    image="https://link-to-image/gang.jpg" />
-
-                <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-                
-                 <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-
-                 <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-
-                
-                <x-song-card 
-                    title="Gang" 
-                    artist="Ayo Maff, Seyi Vibez" 
-                    image="https://link-to-image/gang.jpg" />
-
-                <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-                
-                 <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-
-                 <x-song-card 
-                    title="Lighter" 
-                    artist="A7S, David Guetta, Wizkid" 
-                    image="https://link-to-image/lighter.jpg" />
-            </div>
+        <div class="flex items-center justify-between mb-2">
+            <h2 class="text-2xl font-bold text-white">{{ $title }}</h2>
+            <a href="#" class="text-sm text-gray-400 hover:text-white">Show all</a>
         </div>
+
+        <div class="flex gap-2 overflow-x-scroll scroll-smooth w-full [&::-webkit-scrollbar]:hidden [&::-webkit-scrollbar-track]:hidden [&::-webkit-scrollbar-thumb]:hidden">
+            
+                <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+                
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+
+                    <x-song-card 
+                    title="Song Title" 
+                    artist="Artist name" 
+                    image="#" />
+        
+        </div>
+    </div>
+</div>
