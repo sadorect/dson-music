f1836ccddddddd15c7ddcee6848a98aaf271ca84
sirhmvfx <sirhmvfx@gmail.com>
2025-08-03 09:03:57 +0100

    refactor song card

--- resources/views/components/song-card.blade.php
+++ resources/views/components/song-card.blade.php
@@ -6,8 +6,9 @@
              alt="{{ $title }}" 
              onerror="this.style.display='none';
                     this.parentElement.style.background='linear-gradient(to bottom right, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }})';
-                    this.parentElement.style.backgroundImage='linear-gradient(to bottom right, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}), url(https://source.unsplash.com/1600x900/?{{ urlencode($title) }},{{ urlencode($artist) }})'">
+                    this.parentElement.style.backgroundImage='linear-gradient(to bottom right, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }})';
+                    this.parentElement.style.backgroundImage='linear-gradient(to bottom right, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}, #{{ str_pad(dechex(rand(0, 16777215)), 6, '0', STR_PAD_LEFT) }}), url(https://source.unsplash.com/1600x900/?{{ urlencode($title) }},{{ urlencode($artist) }})';">
     </div>
-    <div class="text-white text-sm font-semibold truncate">{{ $title }}</div>
-    <div class="text-gray-400 text-xs truncate">{{ $artist }}</div>
+    <div class="text-white text-sm truncate">{{ $title }}</div>
+    <div class="text-gray-400 text-xs truncate">Artist Name</div>
 </a>
