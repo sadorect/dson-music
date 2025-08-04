e5c8bd75653ea996495c42eee845cadd8a3a00dd
sirhmvfx <sirhmvfx@gmail.com>
2025-08-04 09:04:39 +0100

    make single artist card route to a page

--- resources/views/components/artist.blade.php
+++ resources/views/components/artist.blade.php
@@ -1,6 +1,6 @@
 @props(['title', 'artist', 'image'])
 
-<div class="w-[200px] p-3 hover:bg-black/10 rounded-md cursor-pointer flex-shrink-0">
+<a href="{{ route('artists.showPublic', $title) }}" class="w-[200px] p-3 hover:bg-black/10 rounded-md cursor-pointer flex-shrink-0">
     <div class="w-full h-48 rounded-full overflow-hidden shadow-md mb-2">
         <img src="{{ $image }}" 
              alt="{{ $title }}" 
@@ -10,4 +10,4 @@
     </div>
     <div class="text-white text-sm font-semibold truncate">{{ $title }}</div>
     <div class="text-gray-400 text-xs truncate">{{ $artist }}</div>
-</div>
+</a>
